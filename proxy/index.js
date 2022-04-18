79c6efd07a1d232e5dd9e8249167f149b1e6cea3
Sergio Latorre <sergio.latorrecaballero.96@gmail.com>
2022-04-18 13:12:10 -0400

    proxy config

--- proxy/index.js
+++ proxy/index.js
@@ -1,7 +1,7 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
+var host = 'localhost';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 5000;
+var port = 5000;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
